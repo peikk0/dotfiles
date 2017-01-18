867fe0b5987bb4acabc52a68db4d264149c2a0e0
Pierre Guinoiseau <pierre@guinoiseau.eu>
2017-01-19 10:19:51 +1300

    xterm/urxvt/st: use Fira Code fonts

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Fira Mono:regular:size=10:antialias=true:autohint=true";
+static char font[] = "Fira Code:regular:size=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
