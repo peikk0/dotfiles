222d11027106720c941660dcaa7457e1a139bfc6
Pierre Guinoiseau <pierre@guinoiseau.nz>
2020-05-03 22:01:34 +1200

    ipython: add config with custom prompt

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,38 @@
+# Configuration file for ipython.
+
+from IPython.terminal.prompts import Prompts, Token
+
+
+class MyPrompts(Prompts):
+    def in_prompt_tokens(self):
+        return [
+            (Token, "\ue73c "),
+            (Token.Prompt, "❯ "),
+        ]
+
+    def continuation_prompt_tokens(self, width=None):
+        if width is None:
+            width = self._width()
+        return [
+            (Token.Prompt, (' ' * (width - 5)) + "  \ue621 "),
+        ]
+
+    def rewrite_prompt_tokens(self):
+        return []
+
+    def out_prompt_tokens(self):
+        return [
+            (Token, "  "),
+            (Token.OutPrompt, "❮ "),
+        ]
+
+
+c.TerminalInteractiveShell.highlighting_style_overrides = {
+    Token.Prompt: 'ansigreen',
+    Token.OutPrompt: 'ansired',
+    Token: 'ansiblue',
+}
+
+c.TerminalInteractiveShell.prompts_class = MyPrompts
+
+c.TerminalIPythonApp.display_banner = False
