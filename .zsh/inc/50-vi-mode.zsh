d14ac2760d1f05ddba6dd5d1299c93473acf7d8e
Pierre Guinoiseau <pierre@guinoiseau.nz>
2021-01-27 14:07:10 +1300

    zsh: add vi-mode plugin

---
+++ .zsh/inc/50-vi-mode.zsh
@@ -0,0 +1,20 @@
+# {{{ Vi Mode
+
+VI_MODE_PLUGIN="${ZDOTDIR:-${HOME}}/.zsh/plugins/vi-mode/zsh-vi-mode.zsh"
+if [[ -f "${VI_MODE_PLUGIN}" ]]; then
+  . "${VI_MODE_PLUGIN}"
+
+  ZVM_KEYTIMEOUT=0.25
+
+  ZVM_VI_ESCAPE_BINDKEY=jj
+
+  ZVM_INSERT_MODE_CURSOR=${ZVM_CURSOR_BLINKING_UNDERLINE}
+  ZVM_NORMAL_MODE_CURSOR=${ZVM_CURSOR_BLOCK}
+  ZVM_VISUAL_MODE_CURSOR=${ZVM_CURSOR_BLOCK}
+  ZVM_VISUAL_LINE_MODE_CURSOR=${ZVM_CURSOR_BLOCK}
+fi
+unset VI_MODE_PLUGIN
+
+# }}}
+
+# vim:filetype=zsh:tabstop=2:shiftwidth=2:fdm=marker:
