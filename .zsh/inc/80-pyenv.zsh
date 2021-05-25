92eeb1d0b7818d2308eab79f9755274afe018c15
Pierre Guinoiseau <pierre@guinoiseau.nz>
2021-05-26 09:57:56 +1200

    zsh: update pyenv load/init for pyenv 2.0

--- .zsh/inc/80-pyenv.zsh
+++ .zsh/inc/80-pyenv.zsh
@@ -3,11 +3,16 @@
 load_pyenv() {
   if [[ -d "${HOME}/.local/pyenv" ]]; then
     PYENV_ROOT="${HOME}/.local/pyenv"
+  elif [[ -d "${HOME}/.pyenv" ]]; then
+    PYENV_ROOT="${HOME}/.pyenv"
+  fi
+  if [[ -n "${PYENV_ROOT}" ]]; then
     path=("${PYENV_ROOT}/bin" ${path})
     export PATH PYENV_ROOT
   fi
 
   if command -v pyenv >/dev/null 2>&1; then
+    eval "$(pyenv init --path)"
     eval "$(pyenv init -)"
   else
     echo "pyenv not found!" >&2
