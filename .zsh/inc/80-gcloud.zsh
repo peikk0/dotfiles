7bed0359a71f756d1924a420ef17ada0a5ecb9b8
Pierre Guinoiseau <pierre@guinoiseau.nz>
2022-12-07 15:36:03 +1300

    zsh: add environment configuration for gcloud

---
+++ .zsh/inc/80-gcloud.zsh
@@ -0,0 +1,18 @@
+
+# {{{ google-cloud-sdk
+
+GOOGLE_CLOUD_SDK="${HOME}/.local/google-cloud-sdk"
+
+if [ -f "${GOOGLE_CLOUD_SDK}/path.zsh.inc" ]; then
+    . "${GOOGLE_CLOUD_SDK}/path.zsh.inc"
+fi
+if [ -f "${GOOGLE_CLOUD_SDK}/completion.zsh.inc" ]; then
+    . "${GOOGLE_CLOUD_SDK}/completion.zsh.inc"
+fi
+
+# For kubectl with GKE
+USE_GKE_GCLOUD_AUTH_PLUGIN=True; export USE_GKE_GCLOUD_AUTH_PLUGIN
+
+# }}}
+
+# vim:filetype=zsh:tabstop=2:shiftwidth=2:fdm=marker:
