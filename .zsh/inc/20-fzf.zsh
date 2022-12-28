967316451f484732afa562a9ad3c0707a9016175
Pierre Guinoiseau <pierre@guinoiseau.nz>
2022-12-28 13:21:41 +1300

    zsh: configure fzf to display the prompt at the top by default

--- .zsh/inc/20-fzf.zsh
+++ .zsh/inc/20-fzf.zsh
@@ -18,7 +18,7 @@ local fzf_colors=(
   --color 'fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C'
   --color 'pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B'
 )
-export FZF_DEFAULT_OPTS="${(j: :)fzf_colors}"
+export FZF_DEFAULT_OPTS="${(j: :)fzf_colors} --layout=reverse"
 
 if [[ -f "${HOME}/.local/fzf/shell/completion.zsh" ]]; then
   . "${HOME}/.local/fzf/shell/completion.zsh"
