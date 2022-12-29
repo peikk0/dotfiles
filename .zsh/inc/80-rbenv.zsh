fb4f70671f88aa6e617dba009a90620585b4a196
Pierre Guinoiseau <pierre@guinoiseau.nz>
2022-12-29 23:03:41 +1300

    zsh: lazy-load rbenv and move it to ~/.local/share/rbenv

--- .zsh/inc/80-rbenv.zsh
+++ .zsh/inc/80-rbenv.zsh
@@ -1,16 +1,27 @@
 # {{{ rbenv
 
-load_rbenv() {
-  if [[ -d "${HOME}/.local/rbenv" ]]; then
-    RBENV_ROOT="${HOME}/.local/rbenv"
+rbenv() {
+  autoload -U colors; colors
+
+  export RBENV_ROOT="${XDG_DATA_HOME:-${HOME}/.local/share}/rbenv"
+
+  if [[ -d "${RBENV_ROOT}/bin" ]]; then
     path=("${RBENV_ROOT}/bin" ${path})
-    export PATH RBENV_ROOT
+    export PATH
   fi
 
-  if command -v rbenv >/dev/null 2>&1; then
-    eval "$(rbenv init -)"
-  else
-    echo "rbenv not found!" >&2
+  if ! command -v rbenv >/dev/null 2>&1; then
+    echo "${fg[red]}\uF00D${reset_color} rbenv not found!" >&2
+    return 1
+  fi
+
+  unset -f rbenv
+  eval "$(rbenv init -)"
+  echo "${fg[green]}\uF00C${reset_color} rbenv loaded!"
+
+  if [[ $# -ge 1 ]]; then
+    echo "${fg[blue]}❯ rbenv $@${reset_color}"
+    rbenv "$@"
   fi
 }
 
