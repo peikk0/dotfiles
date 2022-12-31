e571381712179eed8e1648d1f76023934dd6bb71
Pierre Guinoiseau <pierre@guinoiseau.nz>
2022-12-31 21:27:45 +1300

    zsh: update syntax-highlighting config

--- .zsh/inc/50-syntax-highlighting.zsh
+++ .zsh/inc/50-syntax-highlighting.zsh
@@ -6,16 +6,26 @@ if [[ -f "${_syntax_highlighting_plugin}" ]]; then
   ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
   typeset -A ZSH_HIGHLIGHT_STYLES
-  ZSH_HIGHLIGHT_STYLES[alias]='fg=green,bold'
-  ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=green,bold'
-  ZSH_HIGHLIGHT_STYLES[builtin]='fg=green,bold'
-  ZSH_HIGHLIGHT_STYLES[function]='fg=green,bold'
-  ZSH_HIGHLIGHT_STYLES[command]='fg=green,bold'
-  ZSH_HIGHLIGHT_STYLES[precommand]='fg=green,bold'
-  ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=white,bold'
-  ZSH_HIGHLIGHT_STYLES[redirection]='fg=white,bold'
-  ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=white,bold'
-  ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=white,bold'
+  ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red'
+  ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=blue'
+  ZSH_HIGHLIGHT_STYLES[alias]='fg=green'
+  ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=green'
+  ZSH_HIGHLIGHT_STYLES[builtin]='fg=blue'
+  ZSH_HIGHLIGHT_STYLES[function]='fg=green'
+  ZSH_HIGHLIGHT_STYLES[precommand]='fg=cyan'
+  ZSH_HIGHLIGHT_STYLES[command]='fg=green'
+  ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=cyan'
+  ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=green'
+  ZSH_HIGHLIGHT_STYLES[path]='none'
+  ZSH_HIGHLIGHT_STYLES[globbing]='fg=magenta'
+  ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=green'
+  ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=green'
+  ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=green'
+  ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=blue'
+  ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]='fg=yellow'
+  ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]='fg=yellow'
+  ZSH_HIGHLIGHT_STYLES[assign]='fg=magenta'
+  ZSH_HIGHLIGHT_STYLES[redirection]='fg=cyan'
 fi
 unset _syntax_highlighting_plugin
 
