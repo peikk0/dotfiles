665ec2e6267ac9b7e54f1e6f261bae5fd45ad611
Pierre Guinoiseau <pierre@guinoiseau.nz>
2023-01-11 00:46:44 +1300

    zsh: GKE contexts in kubectx now with icons

--- zsh/inc/80-kube.zsh
+++ zsh/inc/80-kube.zsh
@@ -3,13 +3,17 @@
 alias k="kubectl"
 
 kx() {
-  local _italic="$(tput sitm)"
-  local _fg_blue="$(tput setaf 4)"
-  local _fg_magenta="$(tput setaf 5)"
-  local _fg_cyan="$(tput setaf 6)"
-  local _reset="$(tput sgr0)"
-  # kubectx with tmux popup and shorter context names for GKE
-  local choice="$(_KUBECTX_FORCE_COLOR=1 kubectx | sed -re "s/gke_(.+)_(.+)_(.+)/\\3 ${_italic}${_fg_blue}[GKE:${_fg_cyan}\\1${_fg_blue}:${_fg_magenta}\\2${_fg_blue}]${_reset}/" | fzf-tmux -p 25%,50% --ansi --no-preview)"
+  local \
+    _italic="$(tput sitm)" \
+    _fg_blue="$(tput setaf 4)" \
+    _fg_cyan="$(tput setaf 6)" \
+    _reset="$(tput sgr0)"
+  # kubectx with tmux popup and reformatted context names for GKE
+  local choice="$(
+    _KUBECTX_FORCE_COLOR=1 kubectx \
+      | sed -re "s/gke_(.+)_(.+)_(.+)/\\3 ${_fg_blue} ${_italic}\\1:${_fg_cyan}\\2${_reset}/" \
+      | fzf-tmux -p 25%,50% --ansi --no-preview --prompt '⎈ '
+    )"
   kubectx "$(kubectl config get-contexts -o=name | grep -E "^(gke_.+_)?${choice%% *}\$")"
 }
 
