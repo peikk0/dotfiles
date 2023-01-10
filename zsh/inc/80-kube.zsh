d4ebf80d2f3a29ef57920facfd39ab025763d70a
Pierre Guinoiseau <pierre@guinoiseau.nz>
2023-01-11 01:28:53 +1300

    zsh: change the kx function into a ZLE widget bound to ^K

--- zsh/inc/80-kube.zsh
+++ zsh/inc/80-kube.zsh
@@ -2,21 +2,36 @@
 
 alias k="kubectl"
 
-kx() {
+# CTRL-K - kubectx with tmux popup and reformatted context names for GKE
+fzf-kubectx-widget() {
   local \
     _italic="$(tput sitm)" \
     _fg_blue="$(tput setaf 4)" \
     _fg_cyan="$(tput setaf 6)" \
     _reset="$(tput sgr0)"
-  # kubectx with tmux popup and reformatted context names for GKE
+  setopt localoptions pipefail no_aliases 2>/dev/null
   local choice="$(
     _KUBECTX_FORCE_COLOR=1 kubectx \
       | sed -re "s/gke_(.+)_(.+)_(.+)/\\3 ${_fg_blue} ${_italic}\\1:${_fg_cyan}\\2${_reset}/" \
-      | fzf-tmux -p 25%,50% --ansi --no-preview --prompt '⎈ '
+      | fzf-tmux -p 25%,40% --ansi --no-preview --prompt '⎈ '
     )"
-  [[ -n "${choice}" ]] || return
-  kubectx "$(kubectl config get-contexts -o=name | grep -E "^(gke_.+_)?${choice%% *}\$")"
+  if [[ -z "${choice}" ]]; then
+    zle redisplay
+    return 0
+  fi
+  zle push-line
+  context="$(kubectl config get-contexts -o=name | grep -E "^(gke_.+_)?${choice%% *}\$")"
+  BUFFER="kubectx ${(q)context}"
+  zle accept-line
+  local ret=$?
+  unset choice context
+  zle reset-prompt
+  return $ret
 }
+zle     -N            fzf-kubectx-widget
+bindkey -M emacs '^K' fzf-kubectx-widget
+bindkey -M vicmd '^K' fzf-kubectx-widget
+bindkey -M viins '^K' fzf-kubectx-widget
 
 export KUBECTX_CURRENT_BGCOLOR="$(tput setab 0)"
 export KUBECTX_CURRENT_FGCOLOR="$(tput setaf 6)"
