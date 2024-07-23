79b938a0bb0222e11ad610cc170d0c41540f8211
Pierre Guinoiseau <pierre@guinoiseau.nz>
2024-07-23 13:05:21 +1200

    fish: rewrite kx function to be simpler and cleaner and not use kubectx

--- fish/functions/kx.fish
+++ fish/functions/kx.fish
@@ -1,30 +1,17 @@
+function kube_contexts --description "Returns all kubectl contexts with current context at the top"
+  set -l current_context (kubectl config current-context)
+  echo $current_context
+  kubectl config get-contexts -o name | string match -e -v $current_context
+end
+
 function kx --description "fzf wrapper for kubectx"
-  set -lx _KUBECTX_FORCE_COLOR 1
-
-  set -l choice (
-    kubectx \
-      | sed -r -e "s/gke_(.+)_(.+)_(.+)/\\3 $(set_color blue)󱇶  $(set_color -i blue)\\1:$(set_color cyan)\\2$(set_color normal)/" \
-               -e "s/arn:aws:eks:(.+):(.+):cluster\\/(.+)/\\3 $(set_color blue)  $(set_color -i blue)\\2:$(set_color cyan)\\1$(set_color normal)/" \
-               -e "s/(.+\.teleport\.gitlab\.net)-(.+)/\\2 $(set_color blue)  $(set_color -i blue)\\1$(set_color normal)/" \
-      | fzf-tmux -p 25%,40% --ansi --no-preview --prompt '⎈ '
-    )
-
-  if test -z $choice
-    commandline -f repaint
-    return
-  end
-
-  set -l context
-  if string match -q '*󱇶*' $choice
-    set context (kubectl config get-contexts -o=name | string match -r "^gke_.+_$(string split -f1 ' ' $choice)\$")
-  else if string match -q '**' $choice
-    set context (kubectl config get-contexts -o=name | string match -r "^.+\.teleport\.gitlab\.net-$(string split -f1 ' ' $choice)\$")
-  else if string match -q '**' $choice
-    set context (kubectl config get-contexts -o=name | string match -r "^arn:aws:eks:.+:cluster/$(string split -f1 ' ' $choice)\$")
-  else
-    set context (string split -f1 ' ' $choice)
-  end
-
+  set -l preview '
+    string match -q -r \'^gke_\' {}; and string replace -r \'^gke_(.+)_(.+)_(.+)$\' "$(set_color blue)󱇶  GKE Cluster: $(set_color cyan)\\$3\n$(set_color blue)   GCP Project: $(set_color cyan)\\$1\n$(set_color blue)   Location: $(set_color cyan)\\$2" {}
+    string match -q -r \'^arn:aws:eks:\' {}; and string replace -r \'^arn:aws:eks:(.+):(.+):cluster/(.+)$\' "$(set_color blue)  EKS Cluster: $(set_color cyan)\\$3\n$(set_color blue)   AWS Account ID: $(set_color cyan)\\$2\n$(set_color blue)   Location: $(set_color cyan)\\$1" {}
+    string match -q -r \'^.+\.teleport\.gitlab\.net-:\' {}; and string replace -r \'^(.+\.teleport\.gitlab\.net)-(.+)$\' "$(set_color blue)  Teleport: $(set_color cyan)\\$2\n$(set_color blue)   Proxy: $(set_color cyan)\\$1" {}
+  '
+  set -l context (kube_contexts | fzf --tmux center,25%,40% --no-preview --prompt '⎈ ' --preview $preview --preview-window bottom,3)
+  test -z $context; and return
   kubectx $context
 end
 
