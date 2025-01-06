493b24453afd2ca970712e4e724b1dec9ab47497
Pierre Guinoiseau <pierre@guinoiseau.nz>
2025-01-07 01:23:18 +1300

    fish, starship: workaround empty line when opening a new shell

--- fish/config.fish
+++ fish/config.fish
@@ -403,6 +403,18 @@ if status is-interactive && command -qv starship
   starship init fish | source
   enable_transience
 
+  # Workaround for https://github.com/starship/starship/issues/560#issuecomment-2409922650
+  function starship_transient_prompt_func
+    tput cuu1
+    starship module character
+  end
+
+  function prompt_newline --on-event fish_postexec
+    echo
+  end
+
+  alias clear "command clear; commandline -f clear-screen"
+
 end
 
 # }}}
