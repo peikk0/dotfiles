100951df36910a278f894d6b6796740f80385c1e
Pierre Guinoiseau <pierre@guinoiseau.nz>
2026-03-29 19:05:35 +1300

    i3, polybar: decrease polybar opacity to 75%, and add offset all around

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+xrandr_info=$(xrandr --query | sed -nre 's/^([A-Z0-9-]+) connected primary ([0-9]+)x([0-9]+)\+.+$/\1 \2 \3/p')
+read -r monitor width height <<<"${xrandr_info}"
+
+export MONITOR="${monitor}"
+export BAR_WIDTH=$(( width - 16 ))
 
 polybar-msg cmd quit >/dev/null 2>&1
 nohup polybar > "${XDG_CACHE_HOME:-${HOME}/.cache}/polybar.log" 2>&1 &
