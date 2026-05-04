3b5243c0a0357913788638646e10509fa058e45a
Pierre Guinoiseau <pierre@guinoiseau.nz>
2026-05-04 19:22:00 +1200

    fish: update kx function to stop using kubectx

--- fish/functions/kx.fish
+++ fish/functions/kx.fish
@@ -4,7 +4,7 @@ function kube_contexts --description "Returns all kubectl contexts with current 
   kubectl config get-contexts -o name | string match -e -v $current_context
 end
 
-function kx --description "fzf wrapper for kubectx"
+function kx --description "kubectx-like Kubernetes context switcher"
   set -l preview '
     string match -q -r \'^gke_\' {}; and string replace -r \'^gke_(.+)_(.+)_(.+)$\' "$(set_color blue)󱇶  GKE Cluster: $(set_color cyan)\\$3\n$(set_color blue)   GCP Project: $(set_color cyan)\\$1\n$(set_color blue)   Location: $(set_color cyan)\\$2" {}
     string match -q -r \'^arn:aws:eks:\' {}; and string replace -r \'^arn:aws:eks:(.+):(.+):cluster/(.+)$\' "$(set_color blue)  EKS Cluster: $(set_color cyan)\\$3\n$(set_color blue)   AWS Account ID: $(set_color cyan)\\$2\n$(set_color blue)   Location: $(set_color cyan)\\$1" {}
@@ -12,7 +12,7 @@ function kx --description "fzf wrapper for kubectx"
   '
   set -l context (kube_contexts | fzf --tmux center,60%,40% --prompt ' ' --preview $preview --preview-window bottom,3)
   test -z $context; and return
-  kubectx $context
+  kubectl config use-context $context
 end
 
 # vim:filetype=fish:tabstop=2:shiftwidth=2:fdm=marker:
